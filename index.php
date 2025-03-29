ccd67e891e7cf2c0df1d0260075606a9d1a6c4fa
joaovitorkilson <joaovitorkilson@gmail.com>
2025-03-29 18:36:32 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Base</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <section class="login">
+            <h1>Login</h1>
+            <form class="form" method="POST" action="auth.php">
+                <input type="text" name="email" id="email" placeholder="E-mail">
+                <input type="text" name="password" id="password" placeholder="Senha">
+                <button>Login</button>
+            </form>
+        </div>
+    </section>
+</body>
+</html>
